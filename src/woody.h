340810103f1137ea2a5229fd4f1c09e586abf2df
fde-capu <flavio@carrara.us>
2024-05-29 15:05:39 -0300

    woody.h changes for the next commits: void* data is not char* data, ctype.h for isprint(), prototypes die, hex_dump, lin_dump and say, macros __die and stupid bool implementation.

--- src/woody.h
+++ src/woody.h
@@ -1,23 +1,34 @@
 #ifndef WOODY_H
 #define WOODY_H
+
 # include <elf.h>
 # include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <fcntl.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <ctype.h>
 
 typedef struct	s_elf
 {
 	uint64_t	*key;
-	void		*data;
+	char		*data; // raw bin content
 	Elf64_Phdr	*code;
 	Elf64_Shdr	*text;
 	ssize_t		filesize;
 	int			decrypt;
 }               t_elf;	  
 
-off_t    		get_filesize(int fd);
-void            read_file(t_elf *elf, char *filename);
-void            print_error(char* message);
-void            *ft_memset(void *s, int c, size_t size);
+off_t	get_filesize(int fd);
+void	read_file(t_elf *elf, char *filename);
+void	die(char* message);
+void	*ft_memset(void *s, unsigned char c, size_t size);
+void	hex_dump(void* address, size_t offset);
+void	lin_dump(void* address, size_t offset);
+void	say(char* msg, void* data, char type);
+
+# define ___die(_bool, _msg) if (_bool) die(_msg)
+# define bool char
+# define true 1
+# define false 0
+
 #endif
