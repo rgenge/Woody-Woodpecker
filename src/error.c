9cfea87fd6612a26ea6985385cbcfa90527fb650
fde-capu <flavio@carrara.us>
2024-05-29 15:07:22 -0300

    Implemented close_all_fds(), die() and say().

--- src/error.c
+++ src/error.c
@@ -1,7 +1,20 @@
 #include "woody.h"
 
-void print_error(char* message)
+void close_all_fds()
 {
-    printf("%s",message);
+	for (size_t i = 3; i < 1024; i++)
+	  close(i);
+}
+
+void die(char* message)
+{
+	close_all_fds();
+    perror(message);
     exit(1);
+}
+
+void say(char* msg, void* data, char type)
+{
+  if (type == 'i') // int
+	printf("%s %d\n", msg, *(int*)data);
 }
