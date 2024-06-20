eedcc03e03c500e20b8062817280959b1eebc168
fde-capu <flavio@carrara.us>
2024-06-20 20:39:14 -0300

    Removed samples 32-bit.

--- samples/tiny.sh
+++ samples/tiny.sh
@@ -2,12 +2,6 @@
 
 mkdir -p dumps
 
-gcc -m64 tinyA.c -o A64
-gcc -m64 tinyB.c -o B64
-echo -n "Test A B 64: "
-./A64 && ./B64
-echo
-
 gcc -m64 return.c -o return64
 echo -n "Test return 64 (42): "
 ./return64
